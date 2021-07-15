b9ef7e88fc33c070ac6dc5425823065413869910
airycanon <airycanon@airycanon.me>
2021-07-15 12:20:50 +0800

    Support rule providers

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,12 @@ struct Config {
     remote: String,
     port: i32,
     token: String,
+    #[serde(default)]
     proxies: Vec<Proxy>,
+    #[serde(default)]
     rules: Vec<String>,
+    #[serde(default, rename(deserialize = "rule-providers", serialize = "rule-providers"))]
+    rule_providers: BTreeMap<String, RuleProvider>,
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone, Default)]
@@ -25,10 +29,46 @@ struct Proxy {
     port: i32,
 }
 
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone, Default)]
+struct RuleProvider {
+    #[serde(rename(deserialize = "type", serialize = "type"))]
+    protocol: String,
+    behavior: String,
+    url: String,
+    path: String,
+    interval: i32,
+}
+
+impl Proxy {
+    pub fn to_map(self) -> Mapping {
+        let mut map = Mapping::new();
+        map.insert(Value::String("name".to_string()), Value::String(self.name));
+        map.insert(Value::String("type".to_string()), Value::String(self.protocol));
+        map.insert(Value::String("username".to_string()), Value::String(self.username));
+        map.insert(Value::String("password".to_string()), Value::String(self.password));
+        map.insert(Value::String("server".to_string()), Value::String(self.server));
+        map.insert(Value::String("port".to_string()), Value::Number(Number::from(self.port)));
+
+        map
+    }
+}
+
+impl RuleProvider {
+    pub fn to_map(self) -> Mapping {
+        let mut map = Mapping::new();
+        map.insert(Value::String("type".to_string()), Value::String(self.protocol));
+        map.insert(Value::String("behavior".to_string()), Value::String(self.behavior));
+        map.insert(Value::String("url".to_string()), Value::String(self.url));
+        map.insert(Value::String("path".to_string()), Value::String(self.path));
+        map.insert(Value::String("interval".to_string()), Value::Number(Number::from(self.interval)));
+
+        map
+    }
+}
+
 
 #[get("/config.yaml")]
 async fn index(config: web::Data<Config>) -> impl Responder {
-
     // load remote config
     let client = Client::default();
 
@@ -43,19 +83,14 @@ async fn index(config: web::Data<Config>) -> impl Responder {
     let mut map: BTreeMap<String, Value> = serde_yaml::from_str(body.as_str()).unwrap();
 
     // prepend proxies
-    let proxies = map["proxies"].as_sequence().unwrap();
+    let mut proxies = vec![];
+    if map.contains_key("proxies") {
+        proxies = map["proxies"].as_sequence().unwrap().to_owned();
+    }
+
     let mut new_proxies = vec![];
-
     for proxy in config.proxies.clone().into_iter() {
-        let mut map = Mapping::new();
-        map.insert(Value::String("name".to_string()), Value::String(proxy.name));
-        map.insert(Value::String("type".to_string()), Value::String(proxy.protocol));
-        map.insert(Value::String("username".to_string()), Value::String(proxy.username));
-        map.insert(Value::String("password".to_string()), Value::String(proxy.password));
-        map.insert(Value::String("server".to_string()), Value::String(proxy.server));
-        map.insert(Value::String("port".to_string()), Value::Number(Number::from(proxy.port)));
-
-        new_proxies.push(Value::Mapping(map));
+        new_proxies.push(Value::Mapping(proxy.to_map()));
 
         for v in proxies.clone().into_iter() {
             new_proxies.push(v)
@@ -64,7 +99,11 @@ async fn index(config: web::Data<Config>) -> impl Responder {
     map.insert("proxies".to_string(), Value::Sequence(new_proxies));
 
     // prepend rules
-    let rules = map["proxies"].as_sequence().unwrap();
+    let mut rules = vec![];
+    if map.contains_key("rules") {
+        rules = map["rules"].as_sequence().unwrap().to_owned();
+    }
+
     let mut new_rules = vec![];
     for rule in config.rules.clone().into_iter() {
         new_rules.push(Value::String(rule));
@@ -75,7 +114,22 @@ async fn index(config: web::Data<Config>) -> impl Responder {
     }
     map.insert("rules".to_string(), Value::Sequence(new_rules));
 
-    // todo prepend rule providers
+    // prepend rule-providers
+    let mut rule_providers = Mapping::new();
+    if map.contains_key("rule-providers") {
+        rule_providers = map["rule-providers"].as_mapping().unwrap().to_owned();
+    }
+
+    let mut new_rule_providers = Mapping::new();
+    for (k, v) in config.rule_providers.clone().into_iter() {
+        new_rule_providers.insert(Value::String(k), Value::Mapping(v.to_map()));
+
+        for (k, v) in rule_providers.clone().into_iter() {
+            new_rule_providers.insert(k, v);
+        }
+    }
+    map.insert("rule-providers".to_string(), Value::Mapping(new_rule_providers));
+
     let yaml = serde_yaml::to_string(&map).unwrap();
     HttpResponse::Ok().content_type("text/plain; charset=utf-8").body(yaml)
 }
@@ -88,7 +142,6 @@ async fn main() -> std::io::Result<()> {
     let config: Config = serde_yaml::from_str(contents.as_str()).unwrap_or_default();
 
     let addr = format!("127.0.0.1:{}", config.port);
-
     println!("start server at {}", addr);
 
     HttpServer::new(move || App::new()
